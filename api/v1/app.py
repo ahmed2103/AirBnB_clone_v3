7ef172517a82f3b8d24d8018f6023dfa6fc85f97
Hasona <hasona@kali.kali>
2024-04-27 22:04:39 +0300

    class as class

--- api/v1/app.py
+++ api/v1/app.py
@@ -4,13 +4,16 @@ from models import storage
 from api.v1.views import app_views
 from os import getenv
 from flask import Flask
+from flask_cors import CORS
+
 
 
 app = Flask(__name__)
+CORS(app, resources={r"/api/*": {"origins": "0.0.0.0"}})
 app.register_blueprint(app_views)
 
 @app.teardown_appcontext
-def teardown(self):
+def teardown(obj):
     storage.close()
 
 if __name__ == "__main__":
